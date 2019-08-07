dfd0fd5ff7bef7adb19f8dfd2cebdc5138671f38
vlakreeh <riverserver2012@gmail.com>
2019-08-07 10:46:56 -0400

    more production debugging!

--- src/main/java/net/fabricmc/loom/providers/MinecraftAssetsProvider.java
+++ src/main/java/net/fabricmc/loom/providers/MinecraftAssetsProvider.java
@@ -48,7 +48,7 @@ public class MinecraftAssetsProvider {
 		LoomGradleExtension extension = project.getExtensions().getByType(LoomGradleExtension.class);
 		boolean offline = project.getGradle().getStartParameter().isOffline();
 
-		project.getLogger().lifecycle(":current dir (" + new File(".").getAbsolutePath() + ")");
+		project.getLogger().lifecycle(":current dir (" + new File("").getAbsolutePath() + ")");
 
 		MinecraftVersionInfo versionInfo = minecraftProvider.versionInfo;
 		MinecraftVersionInfo.AssetIndex assetIndex = versionInfo.assetIndex;
